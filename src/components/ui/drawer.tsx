72e9bd668c82538841e8a358f51259f80f76d361
anbanks <anbanks@users.noreply.github.com>
2026-04-19 11:34:57 -0300

    Revert "fix: default shouldScaleBackground=false in Drawer wrapper"
    This reverts commit f4715b6fb6b557f376344ad62040006d61e80171.

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -6,10 +6,9 @@ import { Drawer as DrawerPrimitive } from "vaul"
 import { cn } from "@/lib/utils"
 
 function Drawer({
-  shouldScaleBackground = false,
   ...props
 }: React.ComponentProps<typeof DrawerPrimitive.Root>) {
-  return <DrawerPrimitive.Root data-slot="drawer" shouldScaleBackground={shouldScaleBackground} {...props} />
+  return <DrawerPrimitive.Root data-slot="drawer" {...props} />
 }
 
 function DrawerTrigger({
